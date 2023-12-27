735c84ec5a7ec0bc992ff270c1598d89e69feac8
SmashMex1369 <chris1203aless@gmail.com>
2023-12-27 12:10:48 -0600

    productos carrito

--- JS/carrito.js
+++ JS/carrito.js
@@ -34,6 +34,50 @@ function updateCart(){
             var celdaCantidad = fila.insertCell();
             celdaCantidad.textContent = producto.cantidad;
             celdaCantidad.contentEditable = 'false'; 
+
+            var celdaTotal = fila.insertCell();
+            celdaTotal.textContent = '$'+producto.precio * producto.cantidad ;
+
+            celdaCantidad.oninput = function() {
+                var cantidad = Number(celdaCantidad.textContent);
+                var precio = Number(celdaPrecio.textContent);
+            
+                var total = cantidad * precio;
+                celdaTotal.textContent = total.toFixed(2);
+            }
+
+
+            var celdaAcciones = fila.insertCell();
+            var botonEliminar = document.createElement('button');
+            botonEliminar.textContent = 'Eliminar';
+            botonEliminar.addEventListener('click', function() {
+                eliminarDelCarrito(producto.idProducto);
+            });
+            celdaAcciones.appendChild(botonEliminar);
         });
     })
+    .catch(error => {
+        console.error(error);
+    });
+}
+
+function realizarCheckout(){
+    alert('¡Muchas gracias por su compra!\nSe a descargado su recibo de compra');
+    var fechaHora = new Date().toLocaleString();
+    var inicio =
+    " Abarrotes EL COMPADRE: Los precios que son de la familia\n"+
+    "Recibo de compra\n"+
+    "Fecha y hora de impresion: "+fechaHora+"\n";
+    var final =
+    "\tFue un placer atenderle, vuelva pronto";
+    descargaTicket(inicio, final);
+}
+
+function descargaTicket(){
+    const { jsPDF } = window.jspdf;
+    var doc = new jsPDF();
+
+    doc.text(i, 110, 10, null, null, 'center');
+
+    doc.save("Recibo.pdf");
 }
